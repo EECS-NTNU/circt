c3c436b321db83dfabc9065e552a5da2f4694faa
Magnus Sjalander <magnus.sjalander@ntnu.no>
2024-12-10 17:41:27 +0100

    Reduced CIRCT version with focus on firtool

--- lib/Firtool/Firtool.cpp
+++ lib/Firtool/Firtool.cpp
@@ -10,6 +10,7 @@
 #include "circt/Conversion/Passes.h"
 #include "circt/Dialect/FIRRTL/FIRRTLOps.h"
 #include "circt/Dialect/FIRRTL/Passes.h"
+#include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/HW/HWPasses.h"
 #include "circt/Dialect/OM/OMPasses.h"
 #include "circt/Dialect/SV/SVPasses.h"
